457fa80c2f1c6093482b7d9a4c55f5ba40410981
jorgsiq <jorge.siqueira@yandex.com>
2021-03-24 18:16:23 +0000

    private messages insertion in help command

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,18 @@
+const { DiscordAPIError } = require("discord.js");
+
 const execute = (bot, msg, args) => {
-    msg.author.send('text');
+    const welcomeMessage = new Discord.MessageEmbed()
+        .setColor('#ff9900')
+        .setTitle(`Fala, @${msg.author.username}!`)
+        .setAuthor('nipscon', 'https://cdn.discordapp.com/avatars/408450693524094977/3cd4509e87c1f1a55a6508e91258375c.png?size=512')
+        .setDescription('É bom saber que agora você também faz parte da Đoge Style! \n\nAntes de embarcar nessa jornada não esqueça de ler nossas normas de convivência fixadas no canal de texto #rules \n\nSe precisar de ajuda ou tiver algum problema é só chamar por um @staff em qualquer um dos canais. Espero que se divirta!"')
+        .setImage('https://media1.tenor.com/images/1feaefbc236ec7dead9f225024edc24b/tenor.gif')
+
+    msg.author.send(welcomeMessage);
     return msg.reply(`estou enviando minha lista de truques no seu privado!`);
 }
 
-module.exports ={
+module.exports = {
     name: "help",
     execute,
 }
